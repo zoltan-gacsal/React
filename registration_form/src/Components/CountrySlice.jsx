a2320639474c697c39a17c90eb39edda4e941f01
zoltan-gacsal <gacsalzoltan2017@gmail.com>
2024-01-12 15:25:30 +0100

    dataRequest & reducer

--- registration_form/src/Components/CountrySlice.jsx
+++ registration_form/src/Components/CountrySlice.jsx
@@ -1,2 +1,36 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+//adatok lekérése...
+
+export const dataRequest = createAsyncThunk("country", async ()=> {
+    const country = await fetch('https://backend.ichat.hu/api/countries');
+    return country.json();
+});
+
+
+//reducer elkészítése...
+
+const countryReducer = createSlice({
+    name: 'countries',
+    initialState: {
+        status: null,
+        list: [],
+    },
+
+//extraReducer elkészítése az adatfolyamhoz...
+
+    extraReducers: (build) => {
+        build
+        .addCase(dataRequest.pending, (state) => {
+            state.status = 'Loading'})
+        .addCase(dataRequest.fulfilled, (state, action) => {
+            state.status = 'Ready'
+            state.list = action.payload
+        })
+    }
+});
+
+export default countryReducer.reducer
+
+
 
--- registration_form/src/Components/Store.jsx
+++ registration_form/src/Components/Store.jsx
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import countryReducer from './CountrySlice'
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        countries: countryReducer,
+    },
 })
