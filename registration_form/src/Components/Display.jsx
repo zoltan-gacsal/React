08227f95ef561b2925bd534bc2d96a5a7f9baac0
zoltan-gacsal <gacsalzoltan2017@gmail.com>
2024-01-12 14:49:22 +0100

    Display styled...

--- registration_form/src/Components/Display.jsx
+++ registration_form/src/Components/Display.jsx
@@ -1,18 +1,20 @@
+import './Display.css'
+
 const Display = () => {
   return (
-    <div className="container mt-5" style={{ maxWidth: "800px" }}>
+    <div className=" kontener " >
       <div className="row mb-3">
-        <div className="col">
+        <div className="col-6">
           <label htmlFor="name" className="form-label">
             Name
           </label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" placeholder='Hogy hívnak?' />
         </div>
-        <div className="col">
+        <div className="col-6">
           <label htmlFor="email" className="form-label">
             Email
           </label>
-          <input type="email" className="form-control" />
+          <input type="email" className="form-control" placeholder='Email címed?' />
         </div>
       </div>
 
@@ -34,7 +36,7 @@ const Display = () => {
       <div className="row mb-3">
         <div className="col">
           <label htmlFor="phone" className="form-label">Telefonszám</label>
-          <input type="text" className="form-control"  />          
+          <input type="text" className="form-control" placeholder='Csak számjegyeket adj meg.' />          
         </div>
         <div className="col">
           <label htmlFor="birthday" className="form-label">Születési dátum</label>
@@ -45,26 +47,26 @@ const Display = () => {
       <div className="row mb-3">
         <div className="col">
           <label htmlFor="password" className="form-label">Jelszó</label>
-          <input type="password" className="form-control" />
+          <input type="password" className="form-control" placeholder='Jelszavad' />
         </div>
         <div className="col">
           <label htmlFor="passwordConfirm" className="form-label">Jelszó ismét</label>
-          <input type="password" className="form-control" />
+          <input type="password" className="form-control" placeholder='Jelszavad ismét' />
         </div>
       </div>
 
       <div className="row mb-3">
-        <div className="form-check">
-          <input type="radio" className="form-check-input"  />
-          <label htmlFor="man" className="form-check-label">Férfi</label>
+        <div className="form-check d-flex ">
+          <input type="radio" className="form-check-input" name='gender'  />
+          <label htmlFor="man" className="form-check-label checkbox">Férfi</label>
         </div>
-        <div className="form-check">
-          <input type="radio" className="form-check-input" />
-          <label htmlFor="woman" className="form-check-label">Nő</label>
+        <div className="form-check d-flex">
+          <input type="radio" className="form-check-input" name='gender' />
+          <label htmlFor="woman" className="form-check-label checkbox">Nő</label>
         </div>
-        <div className="form-check">
-          <input type="radio" className="form-check-input" />
-          <label htmlFor="other" className="form-check-label">Egyéb</label>
+        <div className="form-check d-flex">
+          <input type="radio" className="form-check-input" name='gender' />
+          <label htmlFor="other" className="form-check-label checkbox">Egyéb</label>
         </div>
       </div>
 
